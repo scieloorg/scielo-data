a710743899820792f4af9bf5f1572b668f6c3b70
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-04-26 16:16:27 -0300

    Refactor main script

--- getter/raw_getter.py
+++ getter/raw_getter.py
@@ -1,24 +1,35 @@
 import argparse
 import logging
 import os
+import sys
+sys.path.append(os.getcwd())
 
-from sqlalchemy.exc import IntegrityError
-
-from clients.oai_pmh import get_records, record_to_raw_doc
 from datetime import datetime
+from getter.lib.database import upsert_raw
+from getter.lib.oai_pmh import OAIClient, OAIAdapter
+from getter.lib.values import COLLECTION_TO_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model.declarative import RawDocument
 
 
-URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://postgres:alemanha@172.17.0.6:5432/normalized')
-ENGINE = create_engine(URI_RAW_DATA)
-SESSION_FACTOTY = sessionmaker(bind=ENGINE)
+URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://root:alemanha@172.17.0.3:5432/normalized')
+
+try:
+    ENGINE = create_engine(URI_RAW_DATA)
+    SESSION_FACTOTY = sessionmaker(bind=ENGINE)
+except (ConnectionError,
+        ConnectionResetError,
+        ConnectionAbortedError,
+        ConnectionRefusedError) as e:
+    logging.error(e)
 
 
-if __name__ == '__main__':
+def run():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--collection', required=True, choices=COLLECTION_TO_URL.keys())
+    parser.add_argument('-d', '--days_delta', default=30, type=int)
     parser.add_argument('-f', '--from_date', default='')
+    parser.add_argument('-m', '--max_retries', default=3, type=int)
     parser.add_argument('-u', '--until_date', default=datetime.now().strftime('%Y-%m-%d'))
 
     params = parser.parse_args()
@@ -27,26 +38,23 @@ if __name__ == '__main__':
                         format='[%(asctime)s] %(levelname)s %(message)s',
                         datefmt='%d/%b/%Y %H:%M:%S')
 
-    records = get_records(from_date=params.from_date, until_date=params.until_date)
-    raw_documents = map(record_to_raw_doc, records)
+    oai_client = OAIClient(collection=params.collection,
+                           url=COLLECTION_TO_URL.get(params.collection),
+                           max_retries=params.max_retries,
+                           days_delta=params.days_delta)
+    oai_adapter = OAIAdapter(collection=params.collection)
+
+    logging.info('Sending requests to %s' % COLLECTION_TO_URL.get(params.collection))
+    records = oai_client.get_records(from_date=params.from_date,
+                                     until_date=params.until_date)
+
+    raw_documents = map(oai_adapter.get_raw, records)
 
     session = SESSION_FACTOTY()
-
     for rd in raw_documents:
-        try:
-            session.add(rd)
-            session.commit()
-            logging.info('Saved %s' % rd.code)
-        except IntegrityError:
-            session.rollback()
-            existing_record = session.query(RawDocument).filter(RawDocument.gathering_source == rd.gathering_source,
-                                                                RawDocument.collection == rd.collection,
-                                                                RawDocument.code == rd.code).one()
-            if existing_record.datestamp < rd.datestamp:
-                session.delete(existing_record)
-                session.flush()
-                session.add(rd)
-                session.commit()
-                logging.info('Updated %s' % rd.code)
-
+        upsert_raw(session, rd)
     session.close()
+
+
+if __name__ == '__main__':
+    run()
