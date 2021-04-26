42094bf0b64bbc0a44b41f0f6b4b8cf2e5b83cd5
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-04-26 16:14:31 -0300

    Add try except during database creation

--- getter/initialize_database.py
+++ getter/initialize_database.py
@@ -1,12 +1,26 @@
+import logging
 import os
+import sys
+sys.path.append(os.getcwd())
 
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker
-from model.declarative import Base
+from getter.model.declarative import Base
 
 
-URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://postgres:alemanha@172.17.0.6:5432/normalized')
-ENGINE = create_engine(URI_RAW_DATA)
-SESSION_FACTOTY = sessionmaker(bind=ENGINE)
+URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://root:alemanha@172.17.0.3:5432/normalized')
 
-Base.metadata.create_all(ENGINE)
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(asctime)s] %(levelname)s %(message)s',
+                    datefmt='%d/%b/%Y %H:%M:%S')
+
+
+try:
+    ENGINE = create_engine(URI_RAW_DATA)
+    SESSION_FACTOTY = sessionmaker(bind=ENGINE)
+    Base.metadata.create_all(ENGINE)
+
+    logging.info('Database structure successfully created')
+except (ConnectionError, ConnectionResetError, OperationalError) as e:
+    logging.error(e)
