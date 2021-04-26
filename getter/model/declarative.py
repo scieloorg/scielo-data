2f13e18202e2d0ad23668985982f3bc49eb3cf77
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-04-26 16:16:07 -0300

    Remove unnecessary import

--- getter/model/declarative.py
+++ getter/model/declarative.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, VARCHAR, UniqueConstraint, INTEGER, DATE
+from sqlalchemy import Column, VARCHAR, UniqueConstraint, INTEGER
 from sqlalchemy.dialects.postgresql import JSON, TIMESTAMP, ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 
