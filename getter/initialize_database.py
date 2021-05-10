afe8a65402cacba4293123d3da290a4cb66f5a16
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-05-10 15:04:47 -0300

    Encapsula instruções em método main

--- getter/initialize_database.py
+++ getter/initialize_database.py
@@ -3,18 +3,18 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
-from sqlalchemy.orm import sessionmaker
-from getter.model.declarative import Base
+from model.declarative import Base
 
 
 URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://root:alemanha@172.17.0.3:5432/normalized')
 logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%d/%b/%Y %H:%M:%S')
 
-try:
-    ENGINE = create_engine(URI_RAW_DATA)
-    SESSION_FACTOTY = sessionmaker(bind=ENGINE)
-    Base.metadata.create_all(ENGINE)
 
-    logging.info('Database structure successfully created')
-except (ConnectionError, ConnectionResetError, OperationalError) as e:
-    logging.error(e)
+def main():
+    try:
+        ENGINE = create_engine(URI_RAW_DATA)
+        Base.metadata.create_all(ENGINE)
+
+        logging.info('Database structure successfully created')
+    except (ConnectionError, ConnectionResetError, OperationalError) as e:
+        logging.error(e)
