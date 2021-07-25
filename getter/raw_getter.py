76d6be1398b10ea8beca8cafb23458fe8be12c3c
Rafael JPD <rafael.pezzuto@gmail.com>
2021-07-25 14:47:16 -0300

    adiciona parâmetro para permitir obtenção de apenas um registro

--- getter/raw_getter.py
+++ getter/raw_getter.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from datetime import datetime
+from requests.exceptions import HTTPError
 from util.database import upsert_raw
 from util.oai_pmh import OAIClient, OAIAdapter
 from util.values import COLLECTION_TO_URL, METADATA_PREFIXES
@@ -10,7 +11,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://root:alemanha@172.17.0.2:5432/normalized')
+URI_RAW_DATA = os.environ.get('URI_RAW_DATA', 'postgresql://postgres:alemanha@172.17.0.2:5432/normalized')
 
 try:
     ENGINE = create_engine(URI_RAW_DATA)
@@ -27,27 +28,29 @@ def main():
     parser.add_argument('-m', '--max_retries', default=3, type=int)
     parser.add_argument('-u', '--until_date', default=datetime.now().strftime('%Y-%m-%d'))
     parser.add_argument('-p', '--metadata_prefix', default='oai_dc_scielo', choices=METADATA_PREFIXES)
-
+    parser.add_argument('-i', '--identifier')
     params = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG,
-                        format='[%(asctime)s] %(levelname)s %(message)s',
-                        datefmt='%d/%b/%Y %H:%M:%S')
+    logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%d/%b/%Y %H:%M:%S')
 
-    oai_client = OAIClient(collection=params.collection,
-                           url=COLLECTION_TO_URL.get(params.collection),
-                           max_retries=params.max_retries,
-                           days_delta=params.days_delta)
+    oai_client = OAIClient(collection=params.collection, url=COLLECTION_TO_URL.get(params.collection), max_retries=params.max_retries, days_delta=params.days_delta)
     oai_adapter = OAIAdapter(collection=params.collection)
 
-    logging.info('Sending requests to %s' % COLLECTION_TO_URL.get(params.collection))
-    records = oai_client.get_records(metadata_prefix=params.metadata_prefix,
-                                     from_date=params.from_date,
-                                     until_date=params.until_date)
-
-    raw_documents = map(oai_adapter.get_raw, records)
-
     session = SESSION_FACTOTY()
-    for rd in raw_documents:
-        upsert_raw(session, rd)
-    session.close()
+    try:
+        if params.identifier:
+            records = oai_client.get_record(metadata_prefix=params.metadata_prefix, identifier=params.identifier)
+        else:
+            records = oai_client.get_records(metadata_prefix=params.metadata_prefix, from_date=params.from_date, until_date=params.until_date)
+
+        for record in records:
+            raw_doc = oai_adapter.get_raw(record)
+            upsert_raw(session, raw_doc)
+    except HTTPError as e:
+        logging.error(e)
+    finally:
+        session.close()
+
+
+if __name__ == '__main__':
+    main()
