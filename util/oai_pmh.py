f663117808031659eab8f9ff9b0f1a3e5c7de057
Rafael JPD <rafael.pezzuto@gmail.com>
2021-11-28 16:02:10 -0300

    adiciona exception HTTPError

--- util/oai_pmh.py
+++ util/oai_pmh.py
@@ -5,6 +5,7 @@ from model.oai_dc_scielo import SciELORecord
 from sickle import Sickle
 from sickle.oaiexceptions import NoRecordsMatch
 from urllib3.exceptions import MaxRetryError
+from requests.exceptions import HTTPError
 
 
 class OAIClient:
@@ -34,7 +35,15 @@ class OAIClient:
         except NoRecordsMatch:
             logging.info('No records found')
             return []
-        except (ConnectionError, ConnectionResetError, ConnectionAbortedError, ConnectionRefusedError, MaxRetryError, TimeoutError) as e:
+        except (
+            ConnectionError, 
+            ConnectionResetError, 
+            ConnectionAbortedError, 
+            ConnectionRefusedError, 
+            MaxRetryError, 
+            HTTPError,
+            TimeoutError,
+        ) as e:
             logging.error(e)
             return []
 
